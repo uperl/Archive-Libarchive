17d9bf2985837917ca993481829c9ba2f1d4e941
Graham Ollis <plicease@cpan.org>
2021-05-18 10:28:58 -0600

    find the lib

---
+++ lib/Archive/Libarchive/Lib.pm
@@ -0,0 +1,46 @@
+package Archive::Libarchive::Lib;
+
+use strict;
+use warnings;
+use 5.020;
+use FFI::CheckLib 0.28 qw( find_lib_or_die );
+use FFI::Platypus 1.00;
+
+# ABSTRACT: Private class for Archive::Libarchive
+# VERSION
+
+=head1 SYNOPSIS
+
+ perldoc Archive::Libarchive
+
+=head1 DESCRIPTION
+
+There is nothing to see here.  Please see the main documentation page at
+L<Archive::Libarchive>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Archive::Libarchive>
+
+=back
+
+=cut
+
+sub lib
+{
+  find_lib_or_die( lib => 'archive', symbol => [], alien => ['Alien::Libarchive3'] );
+}
+
+sub ffi
+{
+  state $ffi;
+  $ffi ||= do {
+    my $ffi = FFI::Platypus->new( api => 1 );
+    $ffi->lib(__PACKAGE__->lib);
+    $ffi;
+  };
+}
+
+1;
--- t/archive_libarchive.t
+++ t/archive_libarchive.t
@@ -1,5 +1,6 @@
 use Test2::V0 -no_srand => 1;
 use Archive::Libarchive;
+use 5.020;
 
 ok 1, 'todo';
 
